8b71659f20085c0c63a0826ef76430b4cfe3a2c0
eminem98 <carramusaeugenio@gmail.com>
2024-10-23 18:08:44 +0200

    2 - Animation bug fix

--- sections/Home/Hero/Hero.tsx
+++ sections/Home/Hero/Hero.tsx
@@ -32,116 +32,112 @@ const Hero = () => {
     }
   }, [showWords, words.length]);
 
-  // Ferma l'animazione esattamente dopo 5 rotazioni complete (5 secondi)
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsFlipping(false);
-      setStopRotation(true); // Ferma la rotazione esattamente dopo 5 secondi
-    }, 5000); // 5 secondi per 5 rotazioni
+      setStopRotation(true);
+    }, 5000);
     return () => clearTimeout(timer);
   }, []);
 
   return (
-    <div
-      className="bg-tiber text-white w-full flex flex-col items-center justify-center h-[calc(100vh-4.375rem)] md:h-[calc(100vh-5rem)] relative"
-    > 
-      <FadeInSection>
-      <div className="flex flex-col items-center -mt-[4.375rem] md:-mt-20">
+    <div className="bg-tiber text-white w-full flex flex-col items-center justify-center h-[calc(100vh-4.375rem)] md:h-[calc(100vh-5rem)] relative">
+      <div className="flex flex-col items-center -mt-[4.375rem] md:-mt-20 relative z-10">
         <div className={styles.imageWrapper}>
-          <div className={styles.coinContainer}>
-            <div
-              className={`${styles.coin} ${
-                isFlipping ? styles.spin : ''
-              } ${stopRotation ? styles.stopAtBack : ''}`}
-            >
-              <div className={styles.front}>
-                <Image
-                  src="/logos/white-pictogram.png"
-                  alt="White Pictogram"
-                  width={150}
-                  height={150}
-                />
-              </div>
-              <div className={styles.back}>
-                <Image
-                  src="/logos/white-pictogram.png"
-                  alt="Coin Teresi"
-                  width={150}
-                  height={150}
-                />
+          <FadeInSection>
+            <div className={styles.coinContainer}>
+              <div
+                className={`${styles.coin} ${isFlipping ? styles.spin : ''} ${stopRotation ? styles.stopAtBack : ''}`}
+              >
+                <div className={styles.front}>
+                  <Image
+                    src="/logos/white-pictogram.png"
+                    alt="White Pictogram"
+                    width={150}
+                    height={150}
+                  />
+                </div>
+                <div className={styles.back}>
+                  <Image
+                    src="/logos/white-pictogram.png"
+                    alt="Coin Teresi"
+                    width={150}
+                    height={150}
+                  />
+                </div>
               </div>
             </div>
-          </div>
-          {/* Immagini ai lati del logo */}
+          </FadeInSection>
           <Image
             src="/teresi_shape/angelo-personal.png"
             alt="Angelo Personal"
-            width={500}    // Specifica larghezza
-            height={500}   // Specifica altezza
-            className={`${styles.leftImage} hidden md:block ${styles.slideInLeft}`}
+            width={500}
+            height={500}
+            className={`${styles.leftImage} hidden md:block ${styles.slideInLeft} z-20`} // Aggiunto z-index per assicurare la posizione sopra
           />
-
           <Image
             src="/teresi_shape/angelo-estetica.png"
             alt="Angelo Estetica"
-            width={500}    // Specifica larghezza
-            height={500}   // Specifica altezza
-            className={`${styles.rightImage} hidden md:block ${styles.slideInRight}`}
+            width={500}
+            height={500}
+            className={`${styles.rightImage} hidden md:block ${styles.slideInRight} z-20`} // Aggiunto z-index per assicurare la posizione sopra
           />
         </div>
-        
+
         <div className="w-fit flex flex-col items-center justify-center">
-          <div className="text-5xl md:text-7xl">
-            <Typewriter
-              onInit={(typewriter) => {
-                typewriter
-                  .typeString('Dr. Angelo Teresi')
-                  .pauseFor(0)
-                  .callFunction(() => {
-                    const cursorElement = document.querySelector('.Typewriter__cursor');
-                    if (cursorElement) {
-                      cursorElement.classList.add(styles.cursorHidden);
-                    }
-                    setShowWords(true);
-                  })
-                  .start();
-              }}
-              options={{
-                cursor: '|',
-                loop: false,
-                delay: 50,
-              }}
-            />
-          </div>
-        
-          <p className="w-full flex items-center justify-between text-base md:text-2xl">
-            {words.map((word, index) => (
-              <React.Fragment key={index}>
-                {index > 0 && (
+          <FadeInSection>
+            <div className="text-5xl md:text-7xl">
+              <Typewriter
+                onInit={(typewriter) => {
+                  typewriter
+                    .typeString('Dr. Angelo Teresi')
+                    .pauseFor(0)
+                    .callFunction(() => {
+                      const cursorElement = document.querySelector('.Typewriter__cursor');
+                      if (cursorElement) {
+                        cursorElement.classList.add(styles.cursorHidden);
+                      }
+                      setShowWords(true);
+                    })
+                    .start();
+                }}
+                options={{
+                  cursor: '|',
+                  loop: false,
+                  delay: 50,
+                }}
+              />
+            </div>
+          </FadeInSection>
+
+          <FadeInSection className="w-full flex items-center justify-center">
+            <p className="w-full flex items-center justify-between text-base md:text-2xl">
+              {words.map((word, index) => (
+                <React.Fragment key={index}>
+                  {index > 0 && (
+                    <span
+                      className={`font-sans ${visibleWords > index ? styles.fadeIn : styles.hidden}`}
+                      style={{
+                        animationDelay: `${index * 0.5}s`,
+                      }}
+                    >
+                      &bull;
+                    </span>
+                  )}
                   <span
-                    className={`font-sans ${visibleWords > index ? styles.fadeIn : styles.hidden}`}
+                    className={visibleWords > index ? styles.fadeIn : styles.hidden}
                     style={{
                       animationDelay: `${index * 0.5}s`,
                     }}
                   >
-                    &bull;
+                    {word}
                   </span>
-                )}
-                <span
-                  className={visibleWords > index ? styles.fadeIn : styles.hidden}
-                  style={{
-                    animationDelay: `${index * 0.5}s`,
-                  }}
-                >
-                  {word}
-                </span>
-              </React.Fragment>
-            ))}
-          </p>
+                </React.Fragment>
+              ))}
+            </p>
+          </FadeInSection>
         </div>
       </div>
-      </FadeInSection>
-
     </div>
   );
 };
