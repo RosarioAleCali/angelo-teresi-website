c8049a958345cdae5a4b3bd1af7986750433b968
RosarioAleCali <alessandro@kalamos.tech>
2025-10-27 19:35:07 -0400

    Added IVA and updated dependencies

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
 
   return (
     <footer className="bg-tiber p-5 text-center text-white text-sm">
-      &copy; {currentYear} Angelo Teresi | Tutti i Diritti Riservati | 
+      &copy; {currentYear} Angelo Teresi P. IVA 06689160825 | Tutti i Diritti Riservati | 
       <Link href="/privacy-policy" className='hover:underline'>
         &nbsp;Informativa sulla Privacy
       </Link>
